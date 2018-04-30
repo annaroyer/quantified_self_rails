c97d1b65a109b3ea93f062f8f99718e4e846c38a
Anna Royer <anna.royer@colorado.edu>
2018-04-30 16:41:09 -0600

    change relationship between meals and foods to many-to-many relationship

--- db/migrate/20180430214054_add_meal_id_to_food.rb
+++
@@ -1,6 +0,0 @@
-class AddMealIdToFood < ActiveRecord::Migration[5.1]
-  def change
-    add_column :foods, :meal_id, :integer
-    add_index :foods, :meal_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430214054) do
+ActiveRecord::Schema.define(version: 20180430223120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,15 @@ ActiveRecord::Schema.define(version: 20180430214054) do
     t.integer "calories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "meal_id"
-    t.index ["meal_id"], name: "index_foods_on_meal_id"
+  end
+
+  create_table "meal_foods", force: :cascade do |t|
+    t.bigint "meal_id"
+    t.bigint "food_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_meal_foods_on_food_id"
+    t.index ["meal_id"], name: "index_meal_foods_on_meal_id"
   end
 
   create_table "meals", force: :cascade do |t|
@@ -30,4 +37,6 @@ ActiveRecord::Schema.define(version: 20180430214054) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "meal_foods", "foods"
+  add_foreign_key "meal_foods", "meals"
 end
