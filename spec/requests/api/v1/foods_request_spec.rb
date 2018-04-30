e04fb611a1419aa63510bf80bb2bdbdb518547d1
Anna Royer <anna.royer@colorado.edu>
2018-04-30 14:09:41 -0600

    add spec for create a food

--- spec/requests/api/v1/foods_request_spec.rb
+++ spec/requests/api/v1/foods_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Foods API' do
-  context '/api/v1/foods' do
+  context 'get /api/v1/foods' do
     it 'returns all foods currently in the database' do
       create_list(:food, 5)
 
@@ -17,7 +17,7 @@ describe 'Foods API' do
     end
   end
 
-  context '/api/v1/foods/:id' do
+  context 'get /api/v1/foods/:id' do
     it 'returns the food object with the specified id' do
       banana = Food.create!(name: 'Banana', calories: 150)
 
@@ -35,6 +35,22 @@ describe 'Foods API' do
       get '/api/v1/foods/1'
 
       expect(response.status).to eq(404)
+    end
+  end
+
+  context 'post /api/v1/foods' do
+    it 'creates a new food and returns the food item' do
+      create_list(:food, 2)
+      expect(Food.count).to eq(2)
+
+      post '/api/v1/foods', params: { "food": { "name": "Banana", "calories": "150"} }
+
+      banana = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_success
+      expect(banana[:name]).to eq("Banana")
+      expect(banana[:calories]).to eq(150)
+      expect(Food.count).to eq(3)
     end
   end
 end
