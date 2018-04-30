d48436c4dce174cb469be7b0f31d01438c20c244
Anna Royer <anna.royer@colorado.edu>
2018-04-30 16:42:32 -0600

    update meal relationships spec

---
+++ spec/models/meal_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Meal, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  context 'relationships' do
+    it { should have_many :foods }
+    it { should have_many :meal_foods }
+  end
+end
