c78c93ac12a3a0c226eb300c9c0c21bf5738e279
Anna Royer <anna.royer@colorado.edu>
2018-04-30 16:41:45 -0600

    update relationship specs on food spec to match many to many relationship

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -6,4 +6,9 @@ describe Food, type: :model do
     it { should validate_presence_of :calories }
     it { should validate_uniqueness_of :name }
   end
+
+  context 'relationships' do
+    it { should have_many :meal_foods }
+    it { should have_many :meals }
+  end
 end
