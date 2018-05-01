319c95f33a202ea1c40f7c6c4ae1f47200339569
Anna Royer <anna.royer@colorado.edu>
2018-04-30 19:10:40 -0600

    add route and action for meals show

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :foods, except: [:new, :edit]
       resources :meals, only: [:index]
+      namespace :meals, path: 'meals/:meal_id' do
+        resources :foods, only: [:index]
+      end
     end
   end
 end
